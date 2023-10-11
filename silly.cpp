8057a7e7db883d98d1b854f9d7d318286d5aaa94
logprobability <logprobability@outlook.com>
2023-10-11 14:43:21 -0400

    Create silly.cpp

---
+++ silly.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// this file is here to produce a continuous stream of commits.
+// I'm not sure what I'll put here, but stick around and find out with me.
+// If you find this file because you're looking at my github activity, you have done more than 99% of people who will look at streak alone.
+// That means you're not who this is for (unless in the future it does something silly like the name suggests and you like that kind of thing).
+
+// Ideas I have for what this file might be:
+//  - algorithm practice for leetcode type problems (but done backwards, going through std::algorithms, back into popular problem types)
+//  - a workflow-based approach to testing atypical index datastructures. 
