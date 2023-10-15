3719310f1d5c6116aca25fe8ea89ba2f64d1df27
logprobability <logprobability@outlook.com>
2023-10-15 11:59:26 -0400

    Add doctest scaffold

--- silly.cpp
+++ silly.cpp
@@ -13,6 +13,7 @@
 
 #include <algorithm>
 #include <ranges>
+#include <doctest.h>
 
 using std::ranges;
 using std::algorithm;
@@ -32,4 +33,35 @@ int main()
   } while(std::ranges::next_permutation(output, std::greater()).found)
   
   return 0;
+}
+
+// from the doctest tutorial - I have used doctest in the past, but just want to get reacquainted with it after ~2 years
+// (tests have mostly been in python with pybind11 enabling C++ interfacing)
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("testing the factorial function") {
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(2) == 2);
+    CHECK(factorial(3) == 6);
+    CHECK(factorial(10) == 3628800);
+}
+
+TEST_CASE("vectors can be sized and resized") {
+    std::vector<int> v(5);
+
+    REQUIRE(v.size() == 5);
+    REQUIRE(v.capacity() >= 5);
+
+    SUBCASE("adding to the vector increases its size") {
+        v.push_back(1);
+
+        CHECK(v.size() == 6);
+        CHECK(v.capacity() >= 6);
+    }
+    SUBCASE("reserving increases just the capacity") {
+        v.reserve(6);
+
+        CHECK(v.size() == 5);
+        CHECK(v.capacity() >= 6);
+    }
 }
